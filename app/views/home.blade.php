1a2c330d897e25a51a1cb4f836289150eb92de78
Grant Vinson <gvinson@liquidfish.com>
2015-07-31 20:32:42 +0000

    Finished homepage desktop

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -17,18 +17,29 @@
 		<img src="/img/logo.png" srcset="/img/logo.png 1x, /img/logo@2x.png 2x">
 	</section>
 
+	<div class="clearfix"></div>
+
 	<section class="links">
 		<a href="/menu">
 			<img src="/img/icon-menu.png" srcset="/img/icon-menu.png 1x, /img/icon-menu@2x.png 2x">
-			<span>MENU</span>
+			<span>
+				<img src="/img/icon-hover.png" alt="">
+				MENU
+			</span>
 		</a>
 		<a href="/contact">
 			<img src="/img/icon-contact.png" srcset="/img/icon-contact.png 1x, /img/icon-contact@2x.png 2x">
-			<span>CONTACT</span>
+			<span>
+				<img src="/img/icon-hover.png" alt="">
+				CONTACT
+			</span>
 		</a>
 		<a href="/grocery">
 			<img src="/img/icon-grocery.png" srcset="/img/icon-grocery.png 1x, /img/icon-grocery@2x.png 2x">
-			<span>GROCERY</span>
+			<span>
+				<img src="/img/icon-hover.png" alt="">
+				GROCERY
+			</span>
 		</a>
 
 	</section>
